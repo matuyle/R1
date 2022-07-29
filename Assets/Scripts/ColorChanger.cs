ab92076eaf22383f488f45f60cd0277f31052617
Martin Ü <matuyle@gmail.com>
2022-07-29 10:42:40 +0300

    Update ColorChanger.cs

--- Assets/Scripts/ColorChanger.cs
+++ Assets/Scripts/ColorChanger.cs
@@ -7,6 +7,11 @@ public class ColorChanger : MonoBehaviour
     Renderer renderer;
     public Material mat0;
     public Material mat1;
+    
+    private void Start()
+    {
+        renderer = GetComponent<Renderer>();
+    }
 
     void OnCollisionEnter(Collision collision) {
         if (collision.gameObject.tag == "Player") {
